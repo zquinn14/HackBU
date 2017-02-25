e9eb6aa3fb14395cd5310d6c8ddcf1e3f66381f0
Layne Gustafson <laynegustafson97@gmail.com>
2017-02-25 16:35:41 -0500

    Add Word to main

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello world!")
+class Word:
+    
+    def __init__(self, wordstr, syllables):
+        self.wordstr   = wordstr;
+        self.syllables = syllables;
+
+
+w = Word("soda", 2)
