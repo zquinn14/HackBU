e9e553579cca0f755b8a36096fdc58cf5d0c1016
Maitri Mangal <mmmangal1@binghamton.edu>
2017-02-25 20:10:47 -0500

    contains the new file with the parsed words

--- Tester.py
+++ Tester.py
@@ -1,5 +1,19 @@
-def main():
+import re
+def splitWord():
     file = open("Data.txt", "r")
-    print(file)
+    text = file.read()
+    #print(text)
+    text = re.sub('\(|\)' , ' ', text)
+    text = re.sub(',', ' ', text)
+    splitedSong = text.split()
 
-main()
+    writtenfile = open("Edited.txt", "w")
+    writtenfile.write(' '.join(splitedSong))
+    writtenfile.close()
+    file_new = open("Edited.txt", "r")
+    text_new = file_new.read()
+    print(text_new)
+    print(splitedSong)
+    return splitedSong
+
+splitWord()
