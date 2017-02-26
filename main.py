e18b85e575125c91445e6416ede5acca74a70ee6
Layne Gustafson <laynegustafson97@gmail.com>
2017-02-26 08:42:54 -0500

    Update main to run partsofspeech

--- main.py
+++ main.py
@@ -1,33 +1,35 @@
-from Markov import Markov, Word
-import generate
-import tweets
-from wordToSyl import sylco
+# from Markov import Markov, Word
+# import generate
+# import tweets
+# from wordToSyl import sylco
 
-tweets.init()
+# tweets.init()
 
-statuses = tweets.get_statuses("realDonaldTrump")
+# statuses = tweets.get_statuses("realDonaldTrump")
 
-tweetWords = []
-for status in statuses:
-    tweetWord = []
-    for word in status:
-        tweetWord.append(Word(word, sylco(word)))
-    tweetWords.append(tweetWord)
+# tweetWords = []
+# for status in statuses:
+#     tweetWord = []
+#     for word in status:
+#         tweetWord.append(Word(word, sylco(word)))
+#     tweetWords.append(tweetWord)
 
 
-markov = Markov()
+# markov = Markov()
 
-for tweet in tweetWords:
-    if len(tweet) == 0:
-        continue
-    markov.add_to_chain(tweet)
+# for tweet in tweetWords:
+#     if len(tweet) == 0:
+#         continue
+#     markov.add_to_chain(tweet)
 
-songText = generate.read_song()
+# songText = generate.read_song()
 
-output = generate.generate(songText, markov)
+# output = generate.generate(songText, markov)
 
-outputStrings = []
-for word in output:
-    outputStrings.append(word.wordstr)
+# outputStrings = []
+# for word in output:
+#     outputStrings.append(word.wordstr)
 
-print(outputStrings)
+# print(outputStrings)
+
+import partsofspeech
