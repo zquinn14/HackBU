ec98bb2a900bb806c450590adfd8cd43b5cff38d
sagar <skale4@binghamton.edu>
2017-02-26 10:05:24 -0500

    Added Ajax stuff

--- static/js/mymodal.js
+++ static/js/mymodal.js
@@ -9,47 +9,48 @@ jQuery(function($) {
     });
 
     function submitform(){
+        var artistname = $("#artistname").val();
         var songname = $("#songtitle").val();
         var twittername = $("#twitteraccount").val();
         $.ajax({
-            url: "main.py",
+            url: "/generate",
             type: "POST",
-            dataType: 'json',
+            dataType: 'application/json',
             data: {
-                songname:songname
+                song_title:songname,
+                song_artist:artistname,
+                twitter_handle:twittername
+
             },
             success: function(data){
-                $( "#processStat .songolink" ).html('<a class="btn btn-primary" href="#" role="button">Download &raquo;</a>');
-                $( "#processStat .song-o" ).html("Nice to meet you, where you been? I could show you incredible things Magic, madness, heaven, sin Saw you there and I thought Oh my God, look at that face You look like my next mistake Love's a game, want to play? New money, suit and tie I can read you like a magazine Ain't it funny, rumors fly And I know you heard about me So hey, let's be friends I'm dying to see how this one ends Grab your passport and my hand I can make the bad guys good for a weekend");
+                console.log(data);
+                if(!("error" in data)) {
+                    $("#processStat .songolink").html('<a class="btn btn-primary" href="#" role="button">Download &raquo;</a>');
+                    $("#processStat .song-o").html(data['original']);
+                    $("#processStat .songnlink").html('<a class="btn btn-primary" href="#" role="button">Download &raquo;</a>');
+                    $("#processStat .song-n").html(data['generated']);
+                    Success();
+                }
+                else{
+                    Error();
+                }
             },
             error: function (data) {
-                console.log(data);
-                $( ".alert-warning" ).removeClass( "hidden" );
+                Error();
             }
         });
-        // $.ajax({
-        //     type: "POST",
-        //     dataType: 'json',
-        //     data: {
-        //         songname:songname
-        //     },
-        //     success: function(data){
-        //
-        //     },
-        //     error: function (data) {
-        //
-        //     }
-        // });
-        $( "#processStat .songnlink" ).html('<a class="btn btn-primary" href="#" role="button">Download &raquo;</a>');
-        $( "#processStat .song-n" ).html("Nice to meet you, where you been? I could show you incredible things Magic, madness, heaven, sin Saw you there and I thought Oh my God, look at that face You look like my next mistake Love's a game, want to play? New money, suit and tie I can read you like a magazine Ain't it funny, rumors fly And I know you heard about me So hey, let's be friends I'm dying to see how this one ends Grab your passport and my hand I can make the bad guys good for a weekend");
-        Success();
     }
 
     function Success(){
         $( "#processStat" ).removeClass( "hidden" );
     }
 
+    function Error(){
+        $( ".alert-warning" ).removeClass( "hidden" );
+    }
+
     $(".mystart").on('click',function(event) {
         $( "#processStat" ).addClass( "hidden" );
+        $( ".alert-warning" ).addClass( "hidden" );
     })
 });
