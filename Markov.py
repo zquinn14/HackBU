2564d2d7167e97dcd70f9cf27ea6cfd30c126010
Zach <zquinn1@binghamton.edu>
2017-02-25 20:44:46 -0500

    Updated Markov

--- Markov.py
+++ Markov.py
@@ -1,5 +1,7 @@
 import random
 import re
+from tester import splitWord
+
 class markov(object):
     def __init__(self, open_file):
         self.cache = {}
@@ -10,7 +12,7 @@ class markov(object):
 
     def file_to_words(self):
         self.open_file.seek(0)
-        data = self.open_file.read()
+        data = self.splitWord()
         words = data.split()
         return words
 
@@ -28,13 +30,13 @@ class markov(object):
             else:
                 self.cache[start] = [w1]
 
-	def generate_markov_text(self, size=25):
-		begin = random.randint(0, self.word_size-2)
-		seed_word, next_word = self.words[seed], self.words[seed+1]
-		w1, w2 = seed_word, next_word
-		gen_words = []
-		for i in xrange(size):
-			gen_words.append(w1)
-			w1, w2 = w2, random.choice(self.cache[(w1, w2)])
-		gen_words.append(w2)
-		return ' '.join(gen_words)
+    def generate_markov_text(self):
+        begin = random.randint(0, self.word_size-2)
+        seed_word, next_word = self.words[seed], self.words[seed+1]
+        w1, w2 = seed_word, next_word
+	gen_words = []
+	for i in range(len(self.words) - 1):
+		gen_words.append(w1)
+		w1, w2 = w2, random.choice(self.cache[(w1, w2)])
+	gen_words.append(w2)
+	return ' '.join(gen_words)
