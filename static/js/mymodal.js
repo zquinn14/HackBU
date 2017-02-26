6966d5aa8e502bba9e6f092403a0289335eb12b3
sagar <skale4@binghamton.edu>
2017-02-26 12:27:10 -0500

    loader 12:27pm

--- static/js/mymodal.js
+++ static/js/mymodal.js
@@ -9,13 +9,13 @@ jQuery(function($) {
     });
 
     function submitform(){
+        loaderEnable();
         var artistname = $("#artistname").val();
         var songname = $("#songtitle").val();
         var twittername = $("#twitteraccount").val();
         $.ajax({
             url: "/generate",
             type: "POST",
-            dataType: 'application/json',
             data: {
                 song_title:songname,
                 song_artist:artistname,
@@ -23,15 +23,18 @@ jQuery(function($) {
 
             },
             success: function(data){
-                console.log(data);
+                //console.log("Success: " + data);
                 if(!("error" in data)) {
                     $("#processStat .songolink").html('<a class="btn btn-primary" href="#" role="button">Download &raquo;</a>');
                     $("#processStat .song-o").html(data['original']);
+                    $("#processStat .song-titleo").html(songname+" - Original");
                     $("#processStat .songnlink").html('<a class="btn btn-primary" href="#" role="button">Download &raquo;</a>');
                     $("#processStat .song-n").html(data['generated']);
+                    $("#processStat .song-titlen").html(songname+" - Converted");
                     Success();
                 }
                 else{
+                    //console.log("Error: " + data);
                     Error();
                 }
             },
@@ -43,10 +46,22 @@ jQuery(function($) {
 
     function Success(){
         $( "#processStat" ).removeClass( "hidden" );
+        loaderDisable();
     }
 
     function Error(){
         $( ".alert-warning" ).removeClass( "hidden" );
+        loaderDisable();
+    }
+
+    function loaderEnable() {
+        $( ".loader" ).removeClass( "hidden" );
+        $( ".container" ).addClass( "hidden" );
+    }
+
+    function loaderDisable() {
+        $( ".loader" ).addClass( "hidden" );
+        $( ".container" ).removeClass( "hidden" );
     }
 
     $(".mystart").on('click',function(event) {
